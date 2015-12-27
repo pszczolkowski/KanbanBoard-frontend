d9255a127223489e5a567d26c467eebbfe067e42
Krzysztof Pszczółkowski <pszczolkowski.k@gmail.com>
2015-12-27 11:35:13 +0100

    Allow adding new columns

--- app/scripts/boardSettings/board-settings.controller.js
+++ app/scripts/boardSettings/board-settings.controller.js
@@ -7,90 +7,25 @@
 
 	BoardSettingsController.$inject = [
 		'$scope',
-		'Board',
 		'board',
 		'Column',
+		'ColumnCreator',
 		'columns',
-		'identity',
-		'toaster',
-		'UserInvitingModal'];
+		'toaster'];
 
-	function BoardSettingsController($scope, Board, board, Column, columns, identity, toaster, UserInvitingModal) {
-		$scope.board = board;
+	function BoardSettingsController($scope, board, Column, ColumnCreator, columns, toaster) {
 		$scope.columns = columns;
-		$scope.identity = identity;
 		$scope.expandedColumn = {
 			index: null
-			};
-		$scope.addColumn = addColumn;
-		$scope.openUserInvitingModal = openUserInvitingModal;
-		$scope.removeMember = removeMember;
-		$scope.revokeAdministratorRole = revokeAdministratorRole;
-		$scope.grantAdministratorRole = grantAdministratorRole;
+		};
 		$scope.expandColumn = expandColumn;
 		$scope.moveColumnUp = moveColumnUp;
 		$scope.moveColumnDown = moveColumnDown;
+		$scope.openColumnCreator = openColumnCreator;
 
-
-		function addColumn() {
-			var column = new Column();
-			column.name = $scope.columnName;
-			column.boardId = $scope.board.id;
-
-			column.$save();
-		}
-
-		function openUserInvitingModal() {
-			UserInvitingModal.open({
-				board: $scope.board
-			}).then(function () {
-				toaster.pop('success', 'User invited');
-				reloadBoard();
-			});
-		}
-
-		function reloadBoard() {
-			Board.get({
-				boardId: $scope.board.id
-			}).$promise.then(function (board) {
-					$scope.board = board;
-				});
-		}
 
 		function handleError() {
 			toaster.pop('error', 'Some error occurred');
-		}
-
-		function removeMember(user) {
-			Board.removeMember({
-				boardId: $scope.board.id,
-				userId: user.userId
-			}).$promise.then(function () {
-					toaster.pop('success', 'Member has been removed');
-					reloadBoard();
-				}, handleError);
-		}
-
-		function revokeAdministratorRole(member) {
-			Board.setPermissions({
-				memberId: member.id,
-				boardId: $scope.board.id,
-				permissions: 'NORMAL'
-			}).$promise.then(function () {
-					toaster.pop('success', 'Administrator role revoked');
-					reloadBoard();
-				}, handleError);
-		}
-
-		function grantAdministratorRole(member) {
-			Board.setPermissions({
-				memberId: member.id,
-				boardId: $scope.board.id,
-				permissions: 'ADMIN'
-			}).$promise.then(function () {
-					toaster.pop('success', 'Administrator role granted');
-					reloadBoard();
-				}, handleError);
 		}
 
 		function moveColumnUp(index) {
@@ -119,7 +54,7 @@
 
 		function reloadColumns() {
 			var promise = Column.query({
-				boardId: $scope.board.id
+				boardId: board.id
 			}).$promise;
 
 			promise.then(function (columns) {
@@ -135,6 +70,15 @@
 			} else {
 				$scope.expandedColumn.index = index;
 			}
+		}
+
+		function openColumnCreator() {
+			ColumnCreator.open({
+				board: board
+			}).then(function () {
+				toaster.pop('success', 'Column has been added');
+				reloadColumns();
+			});
 		}
 	}
 })();
