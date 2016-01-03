a3ca07b504ddb2d6d1d88868503dedd6d7cdb1ff
Krzysztof Pszczółkowski <pszczolkowski.k@gmail.com>
2016-01-03 10:41:11 +0100

    Add filters to board statistics

--- app/scripts/boardStatistics/boardStatistics.controller.js
+++ app/scripts/boardStatistics/boardStatistics.controller.js
@@ -8,31 +8,138 @@
 	BoardStatisticsController.$inject = [
 		'$scope',
 		'board',
-		'TaskStatistics'];
+		'columns',
+		'labels',
+		'taskFilterFilter',
+		'taskPriority'];
 
-	function BoardStatisticsController($scope, board, TaskStatistics) {
+	function BoardStatisticsController($scope, board, columns, labels, taskFilter, taskPriority) {
 		$scope.chart = {
-			type: 'COLUMN_TYPE'
+			type: 'COLUMN'
 		};
 		$scope.chartTypes = [
-			{name: 'column', value: 'COLUMN_TYPE'},
+			{name: 'column', value: 'COLUMN'},
 			{name: 'label', value: 'LABEL'},
 			{name: 'priority', value: 'PRIORITY'},
-			{name: 'assignee', value: 'ASSIGNING'}
+			{name: 'assignee', value: 'ASSIGNEE'}
 		];
+		$scope.filters = {
+			columnId: null,
+			labelId: null,
+			priority: null,
+			assigneeId: null
+		};
+		$scope.board = board;
+		$scope.boardLabels = labels;
+		$scope.columns = columns;
+		$scope.taskPriorities = taskPriority;
 		$scope.loadStatistics = loadStatistics;
 
 		loadStatistics();
 
 
 		function loadStatistics() {
-			TaskStatistics.getDistribution({
-				boardId: board.id,
-				type: $scope.chart.type
-			}).$promise.then(function (statistics) {
-					$scope.labels = statistics.labels;
-					$scope.values = statistics.values;
-				});
+			switch ($scope.chart.type) {
+				case 'COLUMN':
+					prepareStatisticsDistributedBy('columnId', groupColumnsById());
+					break;
+				case 'LABEL':
+					prepareStatisticsDistributedBy('labelId', groupLabelsById());
+					break;
+				case 'ASSIGNEE':
+					prepareStatisticsDistributedBy('assigneeId', groupMembersById());
+					break;
+				case 'PRIORITY':
+					prepareStatisticsDistributedBy('priority', groupPriorities());
+					break;
+			}
 		}
+
+		function fetchAllTasks() {
+			var allTasks = [];
+			angular.forEach(columns, function (column) {
+				allTasks = allTasks.concat(column.tasks);
+			});
+
+			return allTasks;
+		}
+
+		function groupTasksBy(tasks, field) {
+			var groupedTasks = {};
+
+			angular.forEach(tasks, function (task) {
+				if (!groupedTasks.hasOwnProperty(task[field])) {
+					groupedTasks[task[field]] = [];
+				}
+
+				groupedTasks[task[field]].push(task);
+			});
+
+			return groupedTasks;
+		}
+
+		function prepareChartDataFrom(tasks) {
+			$scope.labels = [];
+			$scope.values = [];
+
+			angular.forEach(tasks, function (tasks, label) {
+				$scope.labels.push(label);
+				$scope.values.push(tasks.length);
+			});
+		}
+
+		function prepareStatisticsDistributedBy(field, labelsGroupedByField) {
+			var tasks = fetchAllTasks();
+			tasks = taskFilter(tasks, $scope.filters);
+			var groupedTasks = groupTasksBy(tasks, field);
+
+			var tasksGroupedByLabels = {};
+			angular.forEach(groupedTasks, function (tasks, field) {
+				tasksGroupedByLabels[labelsGroupedByField[field]] = tasks;
+			});
+
+			prepareChartDataFrom(tasksGroupedByLabels);
+		}
+
+		function groupColumnsById() {
+			var columnsGroupedById = {};
+			angular.forEach(columns, function (column) {
+				columnsGroupedById[column.id] = column.name;
+			});
+
+			return columnsGroupedById;
+		}
+
+		function groupLabelsById() {
+			var labelsGroupedById = {
+				'null': 'Without label'
+			};
+			angular.forEach(labels, function (label) {
+				labelsGroupedById[label.id] = label.name;
+			});
+
+			return labelsGroupedById;
+		}
+
+		function groupMembersById() {
+			var membersGroupedById = {
+				'null': 'Not assigned'
+			};
+			angular.forEach(board.members, function (member) {
+				membersGroupedById[member.userId] = member.username;
+			});
+
+			return membersGroupedById;
+		}
+
+		function groupPriorities() {
+			var groupedPriorities = {};
+			angular.forEach(taskPriority, function (priority) {
+				groupedPriorities[priority.value] = priority.name;
+			});
+
+			return groupedPriorities;
+		}
+
 	}
 })();
