b08ba927fc5d2b0bf61565e929a105ecd3185b2a
Krzysztof Pszczółkowski <pszczolkowski.k@gmail.com>
2016-01-05 09:58:53 +0100

    Add size field to task creator

--- app/scripts/task/task-creator.controller.js
+++ app/scripts/task/task-creator.controller.js
@@ -13,21 +13,25 @@
 		'labels',
 		'Task',
 		'taskPriority',
+		'taskSizes',
 		'toaster'];
 
-	function TaskCreatorController($scope, $uibModalInstance, board, columns, labels, Task, taskPriority, toaster) {
+	function TaskCreatorController($scope, $uibModalInstance, board, columns, labels, Task, taskPriority, taskSizes,
+								   toaster) {
 		$scope.task = {
 			title: '',
 			description: '',
 			columnId: columns[0].id,
 			labelId: null,
 			assigneeId: null,
-			priority: 'LOW'
+			priority: 'LOW',
+			size: 1
 		};
 		$scope.board = board;
 		$scope.columns = columns;
 		$scope.labels = labels;
 		$scope.taskPriorities = taskPriority;
+		$scope.taskSizes = taskSizes;
 		$scope.create = create;
 
 
@@ -39,6 +43,7 @@
 			task.labelId = $scope.task.labelId;
 			task.assigneeId = $scope.task.assigneeId;
 			task.priority = $scope.task.priority.toUpperCase();
+			task.size = parseFloat($scope.task.size);
 
 			task.$save().then(function (createdTask) {
 				$uibModalInstance.close(createdTask);
