89d0cd81136a581a7ac926b0f5875fa1bc16026d
Krzysztof Pszczółkowski <pszczolkowski.k@gmail.com>
2016-01-05 11:38:34 +0100

    Ask if user is sure about exceeding column work in progress limit when editing existing task

--- app/scripts/task/task-details.controller.js
+++ app/scripts/task/task-details.controller.js
@@ -7,6 +7,7 @@
 
 	TaskDetailsController.$inject = [
 		'$scope',
+		'$confirm',
 		'$q',
 		'$uibModalInstance',
 		'board',
@@ -18,7 +19,7 @@
 		'taskSizes',
 		'toaster'];
 
-	function TaskDetailsController($scope, $q, $uibModalInstance, board, columns, labels, Task, task, taskPriority,
+	function TaskDetailsController($scope, $confirm, $q, $uibModalInstance, board, columns, labels, Task, task, taskPriority,
 								   taskSizes, toaster) {
 		$scope.task = angular.copy(task);
 		$scope.board = board;
@@ -34,6 +35,34 @@
 		}
 
 		function save() {
+			if (workInProgressLimitWillBeExceeded()) {
+				$confirm('Updating this task size will exceed column\'s work in progress limit. Are you sure?')
+					.then(saveTask);
+			} else {
+				saveTask();
+			}
+		}
+
+		function workInProgressLimitWillBeExceeded() {
+			var column = getColumnById($scope.task.columnId);
+
+			return column.workInProgressLimit &&
+				!column.workInProgressLimitExceeded &&
+				column.workInProgressLimitType === 'SIZE' &&
+				column.tasksSizeSum + - task.size + $scope.task.size > column.workInProgressLimit;
+		}
+
+		function getColumnById(id) {
+			for (var i = 0; i < columns.length; i++) {
+				if (columns[i].id === id) {
+					return columns[i];
+				}
+			}
+
+			return null;
+		}
+
+		function saveTask() {
 			updateTask()
 				.then(setLabel, handleError)
 				.then(assignUser, handleError)
