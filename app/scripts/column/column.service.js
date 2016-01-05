1db6915294af15b8dd2804985074f0a85855f053
Krzysztof Pszczółkowski <pszczolkowski.k@gmail.com>
2016-01-05 11:04:46 +0100

    Display information about exceeded work in progress limit also for limit set for tasks size

--- app/scripts/column/column.service.js
+++ app/scripts/column/column.service.js
@@ -9,6 +9,13 @@
 
 	function Column($resource, config) {
 		return $resource(config.apiUrl + '/column/:columnId', {}, {
+			query: {
+				transformResponse: transformColumns,
+				isArray: true
+			},
+			get: {
+				transformResponse: transformSingleColumn
+			},
 			update: {
 				url: config.apiUrl + '/column',
 				method: 'PUT'
@@ -22,5 +29,44 @@
 				method: 'POST'
 			}
 		});
+
+
+		function transformColumns(data) {
+			var columns = angular.fromJson(data);
+			for (var i = 0; i < columns.length; i++) {
+				prepareColumn(columns[i]);
+			}
+
+			return columns;
+		}
+
+		function transformSingleColumn(data) {
+			var column = angular.fromJson(data);
+			prepareColumn(column);
+
+			return column;
+		}
+
+		function prepareColumn(column) {
+			column.tasksSizeSum = sumTaskSizes(column.tasks);
+
+			column.workInProgressLimitExceeded = false;
+			if (column.workInProgressLimit) {
+				if (column.workInProgressLimitType === 'QUANTITY' && column.tasks.length > column.workInProgressLimit) {
+					column.workInProgressLimitExceeded = true;
+				} else if (column.workInProgressLimitType === 'SIZE' && column.tasksSizeSum > column.workInProgressLimit) {
+					column.workInProgressLimitExceeded = true;
+				}
+			}
+		}
+
+		function sumTaskSizes(tasks) {
+			var sum = 0;
+			angular.forEach(tasks, function (task) {
+				sum += task.size;
+			});
+
+			return sum;
+		}
 	}
 })();
